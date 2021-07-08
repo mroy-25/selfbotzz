061becab34beec51ad8298908273aa2d1b0dd856
fajar55 <67202122+fajar55@users.noreply.github.com>
2021-07-08 14:46:28 +0700

    Update message.js

--- whatsapp/message.js
+++ whatsapp/message.js
@@ -423,4 +423,13 @@ exports.getbusinessprof = async(jid) => {
             jid: wid,
         }
     }
-
+exports.add = async(orangnya) => {
+orang = orangnya + '@s.whatsapp.net'
+response = await zynn.groupAdd(from, [orang])
+console.log(response.participants)
+for (let user of response.participants.filter(user => Object.values(user)[0].code == 403)) {
+console.log(user)
+let inv = (Object.values(user))
+zynn.sendGroupV4Invite(from, testt, inv[0].invite_code, inv[0].invite_code_exp, false ,`Invitation to join my WhatsApp group`)
+}
+}
