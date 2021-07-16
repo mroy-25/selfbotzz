966ab14deeed5789b5ae223bb68910b4d6a3ebbc
fajar55 <67202122+fajar55@users.noreply.github.com>
2021-07-16 16:47:08 +0700

    Update taka.js

--- taka.js
+++ taka.js
@@ -477,7 +477,7 @@ const flink2 = (img1, img2, url, teks) => {
         thumbnail: img1,
         caption: teks,
         "contextInfo": {
-"mentionedJid: [sender],
+"mentionedJid": [sender],
             text: 'hi',
             "forwardingScore": 1000000000,
             isForwarded: true,
@@ -922,6 +922,7 @@ pp = fakeimage
 zynn.sendMessage(from, help(hitungmundur, ispublic, timee, date, dateIslamic, hit, ucselamat, runtime, run, prefix, wa_version, mcc, mnc, os_version, device_manufacturer, device_model, process, baterai, isday, ttag, ispowersave, isprefix, shep), MessageType.text, {
         thumbnail: '',
         "contextInfo": {
+"mentionedJid": [sender],
             text: 'hi',
             "forwardingScore": 1000000000,
             isForwarded: true,
