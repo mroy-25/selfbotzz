0642dabec217e04132902130b84f286f51778e86
fajar55 <67202122+fajar55@users.noreply.github.com>
2021-08-29 21:17:27 +0700

    Update taka.js

--- taka.js
+++ taka.js
@@ -704,8 +704,24 @@ if(!tod.key.fromMe && !m.isBaileys){
                 mention = typeof(mentionByTag) == 'string' ? [mentionByTag] : mentionByTag
                 mention != undefined ? mention.push(mentionByReply) : []
                 mentionUser = mention != undefined ? mention.filter(n => n) : []
-afk.aefka(from, mentionUser, afkuser, tod)
-afk.endafk(from, sender, afkuser, reply)
+                if (afk.hasOwnProperty(x.split('@')[0])) {
+		    afktime = afk[x.split('@')[0]].afktime
+                    afkreason = afk[x.split('@')[0]].reason
+                    const cekafk = ms(Date.now() - afktime)
+                    ini_txt = `@${x.split('@')[0]} sedang afk.\n\n`
+                    if (afk[x.split('@')[0]] != "") {
+                        ini_txt += `${shp} Reason : ${afkreason}\n${shp} Since : _${cekafk.hours} Jam  ${cekafk.minutes} Menit  ${cekafk.seconds} Detik  Yang lalu_`
+                    }
+wa.Mentions(from, ini_txt, [x], tod)
+                }
+            }
+
+	    //endafk
+	if (afk.hasOwnProperty(orang.split('@')[0])) {
+                reply("Anda telah keluar dari mode afk.")
+                delete afk[orang.split('@')[0]]
+                fs.writeFileSync("./src/afk.json", JSON.stringify(afk))
+            }
 afkuser = JSON.parse(fs.readFileSync('./src/afk.json'))
 }
 if (!publicc){
