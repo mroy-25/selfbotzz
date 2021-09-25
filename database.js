18792b294acd8f5f64a9c59eba98cdd38b8d3323
fajar55 <67202122+fajar55@users.noreply.github.com>
2021-09-25 17:41:57 +0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,52 @@
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://ihsana1234:ihsana1234@cluster0.pudnb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+client.connect(async (error, client) => {
+  if(error) return console.log('Koneksi Gagal')
+  console.log('Koneksi Ke Database Mongo Berhasil')
+    const db = client.db('database_bot');
+
+    //add data
+    exports.adddata = async(data_name, data) => {
+      db.collection(data_name).insertOne(data)
+    }
+    //show data
+    exports.showdata = async(data_name, query) => {
+      return new Promise(async(resolve,reject) => {
+      db.collection(data_name).find(query).toArray((error, result) => {
+      if(error) return reply('error')
+      resolve(result)
+      })
+    })
+    }
+    //delete data
+    exports.delete = async(data_name, data) => {
+      return new Promise(async(resolve,reject) => {
+      db.collection(data_name).deleteOne(data).then((result) => {
+      resolve(result)
+      })
+      .catch((error) => {
+        resolve(error)
+      })
+    })
+    }
+    exports.update = async(data_name, data) => {
+      return new Promise(async(resolve,reject) => {
+      db.collection(data_name).updateOne(data).then((result) => {
+      resolve(result)
+      })
+      .catch((error) => {
+        resolve(error)
+      })
+    })
+    }
+    exports.mute = db.collection('mute')
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { id: 'test' };
+    /*const movie = await mute.find().toArray((error, result) => {
+      console.log(result)
+    })*/
+module.exports.client = client
+});
