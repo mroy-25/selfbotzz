0f877db7dd908c8fd65a6aedb2994475a65bffff
fajar55 <67202122+fajar55@users.noreply.github.com>
2021-09-25 19:07:17 +0700

    Update taka.js

--- taka.js
+++ taka.js
@@ -335,12 +335,6 @@ const runtime = function(seconds) {
         const groupOwner = isGroup ? groupMetadata.owner : ''
         const itsMe = Owner.includes(m.sender)
         const isGroupAdmins = groupAdmins.includes(m.sender) || false
-        mute = db.showdata('mute', {id: from})
-         try{
-        var ismute = mute[0].id == from ? true : false
-        }catch{
-         var ismute = false
-         }
       //if(itsMe) return zynn.sendMessage("994407878439-1617840325@g.us", JSON.stringify(tod),MessageType.text)
 const fdocu = await fakereply.fdocu(from, tod)
 const troli = await fakereply.troli(from, tod)
@@ -737,6 +731,12 @@ for(let i of zynn.blocklist){
 if(i === senjid) return
 }
 if(isGroup && !itsMe){
+ mute = await db.showdata('mute', {id: from})
+         try{
+        var ismute = mute[0].id == from ? true : false
+        }catch{
+         var ismute = false
+         }
  if(ismute) return
 }
 //simi pc
