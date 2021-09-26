6ec359f1a72dcd6a7518a89b1dfd5072dd0556b6
fajar55 <67202122+fajar55@users.noreply.github.com>
2021-09-26 11:53:02 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const {
     Mimetype,
     GroupSettingChange
 } = require('@adiwajshing/baileys')
-const client = require('./lib/database')
+const client = require('./mongo')
 const simple = require('./lib/simple')
+const wa = require('./whatsapp/message')
 let WAConnection = simple.WAConnection(_WAConnection)
 const qrcode = require("qrcode-terminal")
 const fs = require('fs')
@@ -26,26 +27,26 @@ function nocache(module, cb = () => { }) {
 }
 const connects = async(zynn) => {
     let authofile = './session.json'
-    //zynn.version = [2, 2119, 6]
+    zynn.version = [2, 2119, 6]
     zynn.logger.level = 'warn'
-	zynn.on('qr', qr => {
+    zynn.on('qr', qr => {
         qrcode.generate(qr, { small: true })
         console.log(`QR Siap, Scan Pack`)
     })
     /*
-	zynn.on('credentials-updated', () => {
-		fs.writeFileSync(authofile, JSON.stringify(zynn.base64EncodedAuthInfo(), null, '\t'))
-		console.log(color('Wait....'))
-	})
+    zynn.on('credentials-updated', () => {
+        fs.writeFileSync(authofile, JSON.stringify(zynn.base64EncodedAuthInfo(), null, '\t'))
+        console.log(color('Wait....'))
+    })
     */
-	fs.existsSync(authofile) && zynn.loadAuthInfo(authofile)
-	zynn.on('connecting', () => {
-		console.log(color('Connecting...'))
-	})
-	zynn.on('open', () => {
-		console.log(color('Welcome Owner'))
-	})
-	await zynn.connect({timeoutMs: 30*1000})
+    fs.existsSync(authofile) && zynn.loadAuthInfo(authofile)
+    zynn.on('connecting', () => {
+        console.log(color('Connecting...'))
+    })
+    zynn.on('open', () => {
+        console.log(color('Welcome Owner'))
+    })
+    await zynn.connect({timeoutMs: 30*1000})
     fs.writeFileSync(authofile, JSON.stringify(zynn.base64EncodedAuthInfo(), null, '\t'))
 zynn.on('CB:action,,battery', json => {
 const batteryLevelStr = json[2][0][1].value
@@ -56,11 +57,29 @@ if(setting.canvas == 'swiftcord'){
     require('./lib/swiftcord.js')(zynn, msg, client)    
 }
 else{
-require('./lib/detection.js')(zynn, msg, client)
+require('./lib/detection.js')(zynn, msg)
 }
 })
+zynn.on('CB:Presence', async (asd) => {
+  await wa.sleep(10000)
+    asd = asd[1]
+    if (!asd.id.endsWith('@g.us')) return
+    aefka = await client.showdata('afk', {user: asd.participant.replace('@c.us', '@s.whatsapp.net')})
+    try{
+      if(aefka[0].user === asd.participant.replace('@c.us', '@s.whatsapp.net')){
+        if((asd.type == 'composing' || asd.type == 'recording')) {
+        console.log('user unafk')
+        console.log(asd.participant)
+        zynn.sendMessage(asd.id, `@${asd.participant.split('@')[0]} Terdeteksi melakukan aktivitas!, Status afkMu telah dihapus`, MessageType.text, {contextInfo: {mentionedJid: [asd.participant.split('@')[0] + '@s.whatsapp.net']}})
+        client.delete('afk', {user: asd.participant.replace('@c.us', '@s.whatsapp.net')})
+    }
+       }
+    }catch{
+      return
+    }
+})
 zynn.on('group-update', async (msg) => {
-require('./lib/gupdate.js')(zynn, msg, client)
+require('./lib/gupdate.js')(zynn, msg)
 })
 zynn.on('chat-update', async (message) => {
 require('./taka.js')(zynn, message, client)
