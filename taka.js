460cf88ce51daf83d9003436d839cd5e61c52707
fajar55 <67202122+fajar55@users.noreply.github.com>
2021-10-02 22:23:37 +0700

    Update taka.js

--- taka.js
+++ taka.js
@@ -980,18 +980,10 @@ break
 case 's':
 case 'stiker':
         case 'sticker':
-        const stickerdata = {
-        type: 'full',
-        pack: setting.packname,
-        author: 'wa.me/' + sender.split('@')[0],
-        categories: [
-            '🌹'
-        ]
-        }
           if ((isMedia && !tod.message.videoMessage || isQuotedImage) && args.length == 0) {
             const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(tod).replace('quotedM','m')).message.extendedTextMessage.contextInfo : tod
             const media = await zynn.downloadMediaMessage(encmedia)
-            stik = await createSticker(media, stickerdata)
+            stik = await createSticker(media, stickermetadata)
             await wa.sendSticker(from, stik, tod)
           } else if ((isMedia && tod.message.videoMessage.seconds < 11 || isQuotedVideo && tod.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
             const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(tod).replace('quotedM','m')).message.extendedTextMessage.contextInfo : tod
@@ -1013,7 +1005,7 @@ case 'stiker':
                 console.log('Finish')
                 zynn.prepareMessage(from, fs.readFileSync(ran), sticker).then(res => {
                   zynn.downloadMediaMessage(res).then(stik => {
-                    createSticker(stik, stickerdata).then(asu => {
+                    createSticker(stik, stickermetadata).then(asu => {
                       wa.sendSticker(from, asu, tod)
                     })
                   })
